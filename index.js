990b55153c43eadf7b1d3453e45fda4ab19cf801
astro-19 <pushkartas2418@gmail.com>
2022-08-15 21:18:24 +0530

    Nodejs mongoose example

--- index.js
+++ index.js
@@ -8,22 +8,44 @@ const connect = mongoose.connect(url);
 connect.then(() => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    Dishes.create({
         name: 'Uthappizza',
         description: "Test"
-    });
-
-    newDish.save().then((dish) => {
+    }).then((dish) => {
         console.log(dish);
 
-        return Dishes.find({});
-    }).then((dishes) => {
-        console.log(dishes);
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: "Updated Test" }
+        },
+        {
+            new: true
+        }
+        ).exec();
 
-        return Dishes.remove({});
+    }).then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: "i like it :)",
+            author: 'James'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({}).exec();
+
     }).then(() => {
+
         return mongoose.connection.close();
+
     }).catch((err) => {
+
         console.log(err);
+
     });
 });
+
